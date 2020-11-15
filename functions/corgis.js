32293bf93a7b2fe46552aa0e130e99599c03c336
Jason Lengstorf <jason@lengstorf.com>
2020-11-15 15:07:48 -0800

    feat: send thumbnails

--- functions/corgis.js
+++ functions/corgis.js
@@ -9,7 +9,7 @@ exports.handler = async () => ({
       alt: 'a corgi wearing glasses',
       credit: 'Brianna Santellan',
       url:
-        'https://images.unsplash.com/photo-1523567353-71ea31cb9f73?ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=80',
+        'https://images.unsplash.com/photo-1523567353-71ea31cb9f73?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&h=300&q=80',
       name: 'Dögg Barkardóttir',
       favoriteSong: 'Bjärk — Play Dead',
     },
@@ -18,7 +18,7 @@ exports.handler = async () => ({
       alt: 'a corgi looking up at the camera with its tongue out',
       credit: 'Florencia Potter',
       url:
-        'https://images.unsplash.com/photo-1519098901909-b1553a1190af?ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=80',
+        'https://images.unsplash.com/photo-1519098901909-b1553a1190af?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&h=300&q=80',
       name: 'Miz Barkie',
       favoriteSong: 'Corgi B — WAP (Wiggly-Ass Puppy)',
     },
@@ -27,7 +27,7 @@ exports.handler = async () => ({
       alt: 'a moody corgi running in the snow',
       credit: 'Peter Pryharski',
       url:
-        'https://images.unsplash.com/photo-1526137966266-60618b40bcd4?ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=80',
+        'https://images.unsplash.com/photo-1526137966266-60618b40bcd4?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&h=300&q=80',
       name: 'Walden',
       favoriteSong: 'Death Cab For Corgi — I Will Follow You Into The Park',
     },
@@ -36,7 +36,7 @@ exports.handler = async () => ({
       alt: 'a corgi puppy looking up and smiling',
       credit: 'Hanson Lu',
       url:
-        'https://images.unsplash.com/photo-1597806999047-9456837df754?ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=80',
+        'https://images.unsplash.com/photo-1597806999047-9456837df754?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&h=300&q=80',
       name: 'Atticus',
       favoriteSong: 'Furrell Williams — Yappy',
     },
@@ -45,7 +45,7 @@ exports.handler = async () => ({
       alt: 'a corgi puppy sitting on the floor looking at the camera',
       credit: 'Brandon Cormier',
       url:
-        'https://images.unsplash.com/photo-1598894591353-9e7c935dd774?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=80',
+        'https://images.unsplash.com/photo-1598894591353-9e7c935dd774?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&h=300&q=80',
       name: 'Millie',
       favoriteSong: 'Anderson .Pawk — Come Down',
     },
@@ -54,7 +54,7 @@ exports.handler = async () => ({
       alt: 'a corgi sitting in a tent with string lights surrounding it',
       credit: 'Cole Keister',
       url:
-        'https://images.unsplash.com/photo-1546975490-e8b92a360b24?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=80',
+        'https://images.unsplash.com/photo-1546975490-e8b92a360b24?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&h=300&q=80',
       name: 'Admiral Ackbark',
       favoriteSong: 'Splootie & The Blowfish — Only Wanna Steal Your Food',
     },
