6ae40663b6760a4fcf17be1854dbdf04a27d0dc4
Jason Lengstorf <jason@lengstorf.com>
2020-11-18 16:45:47 -0800

    feat: remove duplicated Unsplash data

--- functions/corgis.js
+++ functions/corgis.js
@@ -6,55 +6,31 @@ exports.handler = async () => ({
   body: JSON.stringify([
     {
       id: 'Zwvxj3ytTHc',
-      alt: 'a corgi wearing glasses',
-      credit: 'Brianna Santellan',
-      url:
-        'https://images.unsplash.com/photo-1523567353-71ea31cb9f73?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&h=300&q=80&crop=edges',
       name: 'Dögg Barkardóttir',
       favoriteSong: 'Bjärk — Play Dead',
     },
     {
       id: 'yxmNWxi3wCo',
-      alt: 'a corgi looking up at the camera with its tongue out',
-      credit: 'Florencia Potter',
-      url:
-        'https://images.unsplash.com/photo-1519098901909-b1553a1190af?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&h=300&q=80&crop=edges',
       name: 'Miz Barkie',
       favoriteSong: 'Corgi B — WAP (Wiggly-Ass Puppy)',
     },
     {
       id: 'KQN500iE8KA',
-      alt: 'a moody corgi running in the snow',
-      credit: 'Peter Pryharski',
-      url:
-        'https://images.unsplash.com/photo-1526137966266-60618b40bcd4?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&h=300&q=80&crop=edges',
       name: 'Walden',
       favoriteSong: 'Death Cab For Corgi — I Will Follow You Into The Park',
     },
     {
       id: 'J5feaur-y6I',
-      alt: 'a corgi puppy looking up and smiling',
-      credit: 'Hanson Lu',
-      url:
-        'https://images.unsplash.com/photo-1597806999047-9456837df754?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&h=300&q=80&crop=entropy',
       name: 'Atticus',
       favoriteSong: 'Furrell Williams — Yappy',
     },
     {
       id: 'uc3JhSWITMo',
-      alt: 'a corgi puppy sitting on the floor looking at the camera',
-      credit: 'Brandon Cormier',
-      url:
-        'https://images.unsplash.com/photo-1598894591353-9e7c935dd774?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&h=300&q=80&crop=edges',
       name: 'Millie',
       favoriteSong: 'Anderson .Pawk — Come Down',
     },
     {
       id: 'cX-KEISwDIw',
-      alt: 'a corgi sitting in a tent with string lights surrounding it',
-      credit: 'Cole Keister',
-      url:
-        'https://images.unsplash.com/photo-1546975490-e8b92a360b24?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&h=300&q=80&crop=center',
       name: 'Admiral Ackbark',
       favoriteSong: 'Splootie & The Blowfish — Only Wanna Steal Your Food',
     },
